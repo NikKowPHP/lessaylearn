85b983556dc72611a76cf4f31e552e8547c91591
NikKowPHP <nik.kow@outlook.com>
2024-09-05 15:44:01 +0200

    feat: Update localstorage service with messages

--- lib/services/local_storage_service.dart
+++ lib/services/local_storage_service.dart
@@ -1,5 +1,6 @@
 import 'package:hive/hive.dart';
 import 'package:lessay_learn/features/chat/models/chat_model.dart';
+import 'package:lessay_learn/features/chat/models/message_model.dart';
 import 'package:lessay_learn/services/i_local_storage_service.dart';
 
 
@@ -7,9 +8,13 @@ import 'package:lessay_learn/services/i_local_storage_service.dart';
 class LocalStorageService implements ILocalStorageService {
   static const String _chatsBoxName = 'chats';
   static const String _isLoggedInKey = 'isLoggedIn';
+  static const String _messagesBoxName = 'messages';
 
   Future<Box> _openChatsBox() async {
     return await Hive.openBox(_chatsBoxName);
+  }
+    Future<Box> _openMessagesBox() async {
+    return await Hive.openBox(_messagesBoxName);
   }
 
   @override
@@ -32,5 +37,70 @@ class LocalStorageService implements ILocalStorageService {
     final box = await _openChatsBox();
     final chatList = box.get(_chatsBoxName, defaultValue: []) as List;
     return chatList.map((chat) => ChatModel.fromJson(chat)).toList();
+  }
+
+   @override
+Future<ChatModel?> getChatById(String chatId) async {
+  final chats = await getChats();
+  try {
+    return chats.firstWhere((chat) => chat.id == chatId);
+  } catch (e) {
+    // TODO: Handle the case where no chat with the given ID is found
+    // You can either return null or throw an exception
+    return null; // Or throw an exception: throw ChatNotFoundException();
+  }
+}
+
+  @override
+  Future<void> updateChat(ChatModel chat) async {
+    final chats = await getChats();
+    final index = chats.indexWhere((c) => c.id == chat.id);
+    if (index != -1) {
+      chats[index] = chat;
+      await saveChats(chats);
+    }
+  }
+
+  @override
+  Future<void> deleteChat(String chatId) async {
+    final chats = await getChats();
+    chats.removeWhere((chat) => chat.id == chatId);
+    await saveChats(chats);
+  }
+
+  @override
+  Future<void> saveMessage(MessageModel message) async {
+    final box = await _openMessagesBox();
+    await box.put(message.id, message.toJson());
+  }
+
+  @override
+  Future<List<MessageModel>> getMessagesForChat(String chatId) async {
+    final box = await _openMessagesBox();
+    final allMessages = box.values.map((json) => MessageModel.fromJson(json)).toList();
+    return allMessages.where((message) => message.chatId == chatId).toList();
+  }
+
+  @override
+  Future<MessageModel?> getMessageById(String messageId) async {
+    final box = await _openMessagesBox();
+    final json = box.get(messageId);
+    return json != null ? MessageModel.fromJson(json) : null;
+  }
+
+  @override
+  Future<void> updateMessage(MessageModel message) async {
+    final box = await _openMessagesBox();
+    await box.put(message.id, message.toJson());
+  }
+
+  @override
+  Future<void> deleteMessagesForChat(String chatId) async {
+    final box = await _openMessagesBox();
+    final keysToDelete = box.values
+        .where((json) => MessageModel.fromJson(json).chatId == chatId)
+        .map((json) => MessageModel.fromJson(json).id)
+        .toList();
+    await box.deleteAll(keysToDelete);
   }
 }
