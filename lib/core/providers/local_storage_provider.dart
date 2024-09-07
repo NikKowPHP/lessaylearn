57b5d43bab2a423fb7b44ecb82ae77f08a5b068b
NikKowPHP <nik.kow@outlook.com>
2024-09-07 12:11:42 +0200

    feat: create localstorage provider

---
+++ lib/core/providers/local_storage_provider.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:lessay_learn/services/i_local_storage_service.dart';
+import 'package:lessay_learn/services/local_storage_service.dart';
+
+final localStorageServiceProvider = Provider<ILocalStorageService>((ref) {
+  return LocalStorageService(); // Or your specific implementation
+});
