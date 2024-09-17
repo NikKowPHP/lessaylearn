528d47538cdcd88f6f4c57d49437bffdea49caa5
NikKowPHP <nik.kow@outlook.com>
2024-09-17 09:40:10 +0200

    feat: Add get user by id interface method

--- lib/services/i_user_service.dart
+++ lib/services/i_user_service.dart
@@ -3,4 +3,6 @@ import 'package:lessay_learn/features/chat/models/user_model.dart';
 abstract class IUserService {
   Future<UserModel> getCurrentUser();
   Future<void> updateUser(UserModel user);
+
+  Future<UserModel?> getUserById(String userId);
 }
