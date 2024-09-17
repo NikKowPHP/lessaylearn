d37cf12f7453c513d7559058b12969ac4ae9943c
NikKowPHP <nik.kow@outlook.com>
2024-09-17 11:41:58 +0200

    feat: update user provider with methods for pics comments and likes

--- lib/core/providers/user_provider.dart
+++ lib/core/providers/user_provider.dart
@@ -1,6 +1,9 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:lessay_learn/core/models/comment_model.dart';
+import 'package:lessay_learn/core/models/like_model.dart';
 import 'package:lessay_learn/core/providers/local_storage_provider.dart';
 import 'package:lessay_learn/features/chat/models/user_model.dart';
+import 'package:lessay_learn/features/profile/models/profile_picture_model.dart';
 import 'package:lessay_learn/services/i_user_service.dart';
 import 'package:lessay_learn/services/user_service.dart';
 import 'package:lessay_learn/services/local_storage_service.dart';
@@ -21,4 +24,35 @@ final currentUserProvider = FutureProvider<UserModel>((ref) async {
 final userByIdProvider = FutureProvider.family<UserModel?, String>((ref, userId) async {
   final userService = ref.watch(userServiceProvider);
   return await userService.getUserById(userId);
+});
+
+final userProfilePicturesProvider = FutureProvider.family<List<ProfilePictureModel>, String>((ref, userId) async {
+  final userService = ref.watch(userServiceProvider);
+  return await userService.getUserProfilePictures(userId);
+});
+
+final likesForPictureProvider = FutureProvider.family<List<LikeModel>, String>((ref, pictureId) async {
+  final userService = ref.watch(userServiceProvider);
+  return await userService.getLikesForPicture(pictureId);
+});
+
+final commentsForPictureProvider = FutureProvider.family<List<CommentModel>, String>((ref, pictureId) async {
+  final userService = ref.watch(userServiceProvider);
+  return await userService.getCommentsForPicture(pictureId);
+});
+
+// New providers
+final profilePictureByIdProvider = FutureProvider.family<ProfilePictureModel?, String>((ref, pictureId) async {
+  final userService = ref.watch(userServiceProvider);
+  return await userService.getProfilePictureById(pictureId);
+});
+
+final userLikesProvider = FutureProvider.family<List<LikeModel>, String>((ref, userId) async {
+  final userService = ref.watch(userServiceProvider);
+  return await userService.getUserLikes(userId);
+});
+
+final userCommentsProvider = FutureProvider.family<List<CommentModel>, String>((ref, userId) async {
+  final userService = ref.watch(userServiceProvider);
+  return await userService.getUserComments(userId);
 });
