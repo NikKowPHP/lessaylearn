77e98c6c0292c75373fa4e2a321808a1735d4753
NikKowPHP <nik.kow@outlook.com>
2024-09-17 14:16:15 +0200

    feat: add user lkagnuages provider to the current user provider

--- lib/features/home/providers/current_user_provider.dart
+++ lib/features/home/providers/current_user_provider.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:lessay_learn/core/models/language_model.dart';
 import 'package:lessay_learn/core/providers/local_storage_provider.dart';
 import 'package:lessay_learn/features/chat/models/user_model.dart';
 import 'package:lessay_learn/services/user_service.dart';
@@ -11,4 +12,8 @@ final userServiceProvider = Provider<UserService>((ref) {
 final currentUserProvider = FutureProvider<UserModel>((ref) async {
   final userService = ref.watch(userServiceProvider);
   return userService.getCurrentUser();
+});
+final userLanguagesProvider = FutureProvider.family<List<LanguageModel>, String>((ref, userId) async {
+  final userService = ref.watch(userServiceProvider);
+  return userService.getUserLanguages(userId);
 });
