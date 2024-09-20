a52873ee7a1ab17e20e2cd4663140bd78459a402
NikKowPHP <nik.kow@outlook.com>
2024-09-20 10:12:53 +0200

    refacotr: update new deck form to cover all deck fields

--- lib/features/learn/presentation/screens/add_deck_screen.dart
+++ lib/features/learn/presentation/screens/add_deck_screen.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:lessay_learn/core/models/language_model.dart';
+import 'package:lessay_learn/features/learn/models/deck_model.dart';
 import 'package:lessay_learn/features/learn/presentation/screens/favorite_list_screen.dart';
 import 'package:lessay_learn/features/learn/services/deck_service.dart';
+import 'package:uuid/uuid.dart';
 
 class AddDeckScreen extends ConsumerStatefulWidget {
   @override
@@ -10,8 +12,26 @@ class AddDeckScreen extends ConsumerStatefulWidget {
 }
 
 class _AddDeckScreenState extends ConsumerState<AddDeckScreen> {
+  final _formKey = GlobalKey<FormState>();
+  late TextEditingController _nameController;
+  late TextEditingController _descriptionController;
+  String _languageLevel = 'Beginner';
   LanguageModel? sourceLanguage;
   LanguageModel? targetLanguage;
+
+  @override
+  void initState() {
+    super.initState();
+    _nameController = TextEditingController();
+    _descriptionController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _nameController.dispose();
+    _descriptionController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,26 +40,63 @@ class _AddDeckScreenState extends ConsumerState<AddDeckScreen> {
     return CupertinoPageScaffold(
       navigationBar: CupertinoNavigationBar(
         middle: Text('Add New Deck'),
+        trailing: CupertinoButton(
+          padding: EdgeInsets.zero,
+          child: Text('Save'),
+          onPressed: _saveDeck,
+        ),
       ),
       child: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            CupertinoButton(
-              child: Text('Select Source Language'),
-              onPressed: () => _showLanguageSelector(context, deckService, true),
-            ),
-            CupertinoButton(
-              child: Text('Select Target Language'),
-              onPressed: () => _showLanguageSelector(context, deckService, false),
-            ),
-            CupertinoButton.filled(
-              child: Text('Select Favorites'),
-              onPressed: sourceLanguage != null && targetLanguage != null
-                  ? () => _navigateToFavoriteList(context)
-                  : null,
-            ),
-          ],
+        child: Form(
+          key: _formKey,
+          child: ListView(
+            padding: EdgeInsets.all(16),
+            children: [
+              CupertinoTextField(
+                controller: _nameController,
+                placeholder: 'Deck Name',
+                padding: EdgeInsets.all(12),
+              ),
+              SizedBox(height: 16),
+              CupertinoTextField(
+                controller: _descriptionController,
+                placeholder: 'Description',
+                padding: EdgeInsets.all(12),
+                maxLines: 3,
+              ),
+              SizedBox(height: 16),
+              CupertinoButton(
+                child: Text(sourceLanguage?.name ?? 'Select Source Language'),
+                onPressed: () => _showLanguageSelector(context, deckService, true),
+              ),
+              SizedBox(height: 8),
+              CupertinoButton(
+                child: Text(targetLanguage?.name ?? 'Select Target Language'),
+                onPressed: () => _showLanguageSelector(context, deckService, false),
+              ),
+              SizedBox(height: 16),
+              CupertinoSlidingSegmentedControl<String>(
+                children: {
+                  'Beginner': Text('Beginner'),
+                  'Intermediate': Text('Intermediate'),
+                  'Advanced': Text('Advanced'),
+                },
+                onValueChanged: (value) {
+                  if (value != null) {
+                    setState(() => _languageLevel = value);
+                  }
+                },
+                groupValue: _languageLevel,
+              ),
+              SizedBox(height: 24),
+              CupertinoButton.filled(
+                child: Text('Select Favorites'),
+                onPressed: sourceLanguage != null && targetLanguage != null
+                    ? () => _navigateToFavoriteList(context)
+                    : null,
+              ),
+            ],
+          ),
         ),
       ),
     );
@@ -88,5 +145,24 @@ class _AddDeckScreenState extends ConsumerState<AddDeckScreen> {
         ),
       ),
     );
+  }
+
+  void _saveDeck() {
+    if (_formKey.currentState!.validate()) {
+      final newDeck = DeckModel(
+        id: Uuid().v4(),
+        name: _nameController.text,
+        description: _descriptionController.text,
+        cardCount: 0,
+        lastStudied: DateTime.now(),
+        languageLevel: _languageLevel,
+        sourceLanguage: sourceLanguage!.id,
+        targetLanguage: targetLanguage!.id,
+      );
+      
+      ref.read(deckServiceProvider).addDeck(newDeck).then((_) {
+        Navigator.pop(context);
+      });
+    }
   }
 }
