fd20b0263f6d4a1cbf20e3001ad4d42031aaa3a5
NikKowPHP <nik.kow@outlook.com>
2024-10-08 14:30:36 +0200

    fix: Dipslay Image in review step

--- lib/features/onboarding/presentation/steps/review_step.dart
+++ lib/features/onboarding/presentation/steps/review_step.dart
@@ -1,15 +1,21 @@
 // lib/features/onboarding/presentation/steps/review_step.dart
 
+import 'dart:typed_data';
+
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:lessay_learn/core/providers/user_provider.dart';
 import 'package:lessay_learn/features/chat/models/user_model.dart';
 
-class ReviewStep extends StatelessWidget {
+class ReviewStep extends ConsumerWidget {
   final UserModel user;
 
   const ReviewStep({Key? key, required this.user}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final profilePictureAsync = ref.watch(profilePictureByIdProvider(user.avatarUrl));
+
     return ListView(
       padding: const EdgeInsets.all(16.0),
       children: [
@@ -25,19 +31,30 @@ class ReviewStep extends StatelessWidget {
         _buildInfoRow('Interests', user.interests.join(', ')),
         const SizedBox(height: 16),
        
-          Center(
-            child: Container(
-              width: 100,
-              height: 100,
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                image: DecorationImage(
-                  image: NetworkImage(user.avatarUrl!),
-                  fit: BoxFit.cover,
+            profilePictureAsync.when(
+          data: (profilePicture) {
+            if (profilePicture != null) {
+              Uint8List imageBytes = profilePicture.getImageBytes();
+              return Center(
+                child: Container(
+                  width: 100,
+                  height: 100,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                      image: MemoryImage(imageBytes), // Use MemoryImage to display the image
+                      fit: BoxFit.cover,
+                    ),
+                  ),
                 ),
-              ),
-            ),
-          ),
+              );
+            } else {
+              return const Center(child: Text('No profile picture available.'));
+            }
+          },
+          loading: () => const Center(child: CupertinoActivityIndicator()),
+          error: (e, stack) => Center(child: Text('Error loading profile picture: $e')),
+        ),
       ],
     );
   }
